62ce5b9d01c064c63482818d0f6f50ace1823ccc
handsomehyunsu <rlagustn19944@gmail.com>
2022-07-19 14:17:24 -0700

    frame

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import MasterHeader from "./components/MasterHeader/MasterHeader";
 import Services from "./components/Services/Service";
+import Portfolio from "./components/Portfolio/Portfolio";
+import About from "./components/About/About";
+import Team from "./components/Team/Team";
+import Clients from "./components/Clients/Clients";
+import Contact from "./components/Contact/Contact";
 function App() {
   return (
     <body>
       <Nav></Nav>
       <MasterHeader></MasterHeader>
       <Services></Services>
+      <Portfolio></Portfolio>
+      <About></About>
+      <Team></Team>
+      <Clients></Clients>
+      <Contact></Contact>
     </body>
   );
 }
